ec38f8b0345f4ef764247471b1f13217eb59dbac
Faruq Rasid <me@ruqqq.sg>
2022-06-12 21:09:10 +0800

    Run install-expo-modules

--- android/app/src/main/java/sg/ruqqq/prayertimepro/MainActivity.java
+++ android/app/src/main/java/sg/ruqqq/prayertimepro/MainActivity.java
@@ -1,4 +1,5 @@
 package sg.ruqqq.prayertimepro;
+import expo.modules.ReactActivityDelegateWrapper;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -27,7 +28,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected ReactActivityDelegate createReactActivityDelegate() {
-    return new MainActivityDelegate(this, getMainComponentName());
+    return new ReactActivityDelegateWrapper(this, new MainActivityDelegate(this, getMainComponentName()));
   }
 
   public static class MainActivityDelegate extends ReactActivityDelegate {
