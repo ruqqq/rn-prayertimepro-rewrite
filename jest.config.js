890e9b666dd18ba912399c6ad81bb7f3b578098a
Faruq Rasid <me@ruqqq.sg>
2022-06-12 12:00:20 +0800

    Add RNTL, fix jest+ts setup, add husky + lint-staged

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,12 @@
 module.exports = {
   preset: 'react-native',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  setupFiles: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png)$': 'identity-obj-proxy',
+  },
+  transformIgnorePatterns: [
+    '<rootDir>/node_modules/(?!(@react-native|react-native|react-native-vector-icons)/).*/',
+  ],
 };
