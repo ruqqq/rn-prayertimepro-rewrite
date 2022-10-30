1871e56a3fea72ae2ab4cc6e896f86724fe43131
Faruq Rasid <me@ruqqq.sg>
2022-10-30 22:08:38 +0800

    Implement shared theme between react-navigation and react-native-paper

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,16 @@ import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { Provider as PaperProvider } from 'react-native-paper';
 import Navigation from './Navigation';
 import useAppInitEffect from './useAppInitEffect';
+import { CombinedDarkTheme } from './theme';
 
 const AppContainer = () => {
   const { isReady, initialRouteName } = useAppInitEffect();
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <PaperProvider>
+      <PaperProvider theme={CombinedDarkTheme}>
         {isReady ? (
-          <NavigationContainer>
+          <NavigationContainer theme={CombinedDarkTheme}>
             <Navigation initialRouteName={initialRouteName} />
           </NavigationContainer>
         ) : null}
